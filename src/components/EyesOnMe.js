32bfd265d2551d55e35aa3b8d5f94ba6017564da
fusion407 <fusion407@gmail.com>
2022-10-10 01:00:37 -0500

    complete

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,21 @@
-// Code EyesOnMe Component Here
+import React from 'react'
+
+function EyesOnMe() {
+    function handleBlur(e) {
+        console.log('Hey! Eyes on me!')
+    }
+    function handleFocus(e) {
+        console.log('Good!')
+    }
+    return (
+        <div>
+            <button
+                onBlur={handleBlur}
+                onFocus={handleFocus}>
+            Eyes on me
+            </button>
+        </div>
+        )
+  }
+
+export default EyesOnMe
